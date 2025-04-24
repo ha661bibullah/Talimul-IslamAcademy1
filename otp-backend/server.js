c4d72eac96514e0789885cc344c4488f55494c8e
Arif Billah <ha661bibullah@gmail.com>
2025-04-24 21:38:36 +0600

    Update: major changes in OTP system and bug fixes

--- otp-backend/server.js
+++ otp-backend/server.js
@@ -13,14 +13,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Email transporter setup
-// const transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASSWORD
-//   }
-// });
-
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
@@ -28,7 +20,6 @@ const transporter = nodemailer.createTransport({
     pass: process.env.EMAIL_PASS
   }
 });
-
 
 
 // API endpoint to send OTP
